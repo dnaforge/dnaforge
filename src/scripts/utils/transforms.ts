ad385dd131e1fbbde417cc2df6b97fe2d5f4515a
Antti Elonen <antti.elonen@aalto.fi>
2023-06-26 23:22:03 +0300

    Fixed the buggy fix...

--- src/scripts/utils/transforms.ts
+++ src/scripts/utils/transforms.ts
@@ -14,7 +14,7 @@ export function get2PointTransform(p1: Vector3, p2: Vector3): Matrix4 {
   const center = p1.clone().add(p2).multiplyScalar(0.5);
 
   const dir = p2.clone().sub(p1).normalize();
-  const nor1 = r1.sub(dir.clone().multiplyScalar(r1.dot(dir))).normalize();
+  const nor1 = r1.clone().sub(dir.clone().multiplyScalar(r1.dot(dir))).normalize();
   const nor2 = dir.clone().cross(nor1);
 
   const transform = new Matrix4()
