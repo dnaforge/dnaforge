7d81007d583d0d7d1f6d90a0d275957e99f53359
Antti Elonen <antti.elonen@aalto.fi>
2023-06-26 19:16:07 +0300

    New build. Fixed a small rendering glitch.

--- src/scripts/utils/transforms.ts
+++ src/scripts/utils/transforms.ts
@@ -1,5 +1,7 @@
 import { Matrix4, Vector3 } from 'three';
 
+const r1 = new Vector3().randomDirection();
+
 /**
  * Creates a transformation which transforms a Y-oriented unit vector centered at
  * the origin to a vector that spans from point p1 to p2.
@@ -10,8 +12,6 @@ import { Matrix4, Vector3 } from 'three';
  */
 export function get2PointTransform(p1: Vector3, p2: Vector3): Matrix4 {
   const center = p1.clone().add(p2).multiplyScalar(0.5);
-
-  const r1 = new Vector3(Math.random(), Math.random(), Math.random());
 
   const dir = p2.clone().sub(p1).normalize();
   const nor1 = r1.sub(dir.clone().multiplyScalar(r1.dot(dir))).normalize();
