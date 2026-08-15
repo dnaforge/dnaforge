8360ef01115e2826877da450474857034a6e09b2
orponen <pekka.orponen@aalto.fi>
2026-08-15 20:15:30 +0300

    Update sterna_menu.ts
    Added choice for clockwise and counterclockwise routings

--- src/scripts/modules/sterna/sterna_menu.ts
+++ src/scripts/modules/sterna/sterna_menu.ts
@@ -132,6 +132,9 @@ export class SternaMenu extends ModuleMenu {
     register(this.params, 'minKLsIterations', 'sterna-minkls-iterations');
     register(this.params, 'dfs', 'sterna-depth-tree');
     register(this.params, 'rst', 'sterna-random-tree');
+    register(this.params, 'rst', 'sterna-clockwise');
+    register(this.params, 'rst', 'sterna-counterclockwise');
+
 
     this.generatePrimaryButton = $('#generate-sterna-primary');
 
